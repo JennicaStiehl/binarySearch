2f47ad3d5a1a347ff210597cb0c6de9968b3c677
JennicaStiehl <jennica.stiehl@gmail.com>
2019-10-11 12:06:15 -0600

    Removes unnecessary file.

--- binary.rb
+++ binary.rb
@@ -1,17 +1,3 @@
-
-
-Only you can see this message
-This story's distribution setting is on. You're in the Partner Program, so this story is eligible to earn money. Learn more
-Binary Search
-Jennica Stiehl
-Jennica Stiehl
-Sep 9 · 2 min read
-
-An Algorithm
-Let’s say you are looking up the word ‘jade’ in the dictionary. Would it make more sense to start in the ‘a’ section and check each section or start in the middle and search until you find the ‘j’ section? Probably the middle, right? Yes.
-So, a binary search cuts the dictionary in half on each iteration or time you look until the word is found. So, if you were looking for a number between 1 and 1024, you would look 10 times, instead of the 1024 (if your number was the last possible number). It is important to note that a binary search only works on an ordered list.
-Examples
-Here is an example in Ruby:
 class Search
 def binary_search(list, item)
     low = 0
